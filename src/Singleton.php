43b2cce7cd3e94620f690959835d527966185d09
Jakub Mikita <jakub@bracketspace.com>
2020-01-06 19:26:47 +0100

    project init

---
+++ src/Singleton.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Singleton class
+ *
+ * @package micropackage/singleton
+ */
+
+namespace Micropackage\Singleton;
+
+/**
+ * Singleton class
+ */
+class Singleton {
+
+	/**
+	 * Object instances
+	 *
+	 * @var array
+	 */
+	protected static $instances = [];
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 */
+	protected function __construct() {}
+
+	/**
+	 * Clone method
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	protected function __clone() {}
+
+	/**
+	 * Wakeup method
+	 *
+	 * @since 1.0.0
+	 * @throws \Exception When used.
+	 */
+	public function __wakeup() {
+		throw new \Exception( 'Cannot unserialize singleton' );
+	}
+
+	/**
+	 * Gets the instance
+	 *
+	 * @since  1.0.0
+	 * @return Singleton
+	 */
+	public static function get() {
+		$class = get_called_class();
+
+		if ( ! isset( self::$instances[ $class ] ) ) {
+			self::$instances[ $class ] = new static();
+		}
+
+		return self::$instances[ $class ];
+	}
+
+}
